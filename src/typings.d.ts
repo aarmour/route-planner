72909f730a3717eba59113dd653e2728bc65178b
Adam Armour <adam.armour@teamaol.com>
2016-06-28 14:57:00 -0600

    chore: remove mapbox-gl typings

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,5 +1,3 @@
 /// <reference path="../typings/browser.d.ts" />
-/// <reference path="../typings/globals/geojson/index.d.ts" />
-/// <reference path="../typings/modules/mapbox-gl/index.d.ts" />
 
 declare var module: { id: string };
